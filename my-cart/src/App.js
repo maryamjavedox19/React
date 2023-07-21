d9f17e18d94a6f8bc721335e8ccbf5271233ef6c
Maryam <maryamjaved.xo@gmail.com>
2023-07-21 15:16:44 +0500

    navbar component

--- my-cart/src/App.js
+++ my-cart/src/App.js
@@ -2,10 +2,12 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+    <Navbar/>
     </>
    );
 
